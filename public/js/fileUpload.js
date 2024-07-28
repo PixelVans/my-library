d91daf01e0913e7152ea1f19f118a1e428044dc8
PixelVans <evansmemba001@gmail.com>
2024-07-29 01:42:22 +0300

    final edit

--- public/js/fileUpload.js
+++ public/js/fileUpload.js
@@ -1,4 +1,27 @@
-// Register FilePond plugins
+const rootStyles = window.getComputedStyle
+    (document.documentElement)
+
+if (rootStyles.getPropertyValue("--book-cover-width-large") !== null
+&& rootStyles.getPropertyValue("--book-cover-width-large") !== '')
+{
+    ready()
+} else {
+    document.getElementById("main-css")
+    .addEventListener("load", ready)
+    }
+
+
+function ready() {
+
+    const coverwidth = parseFloat(rootStyles.getPropertyValue
+        ("--book-cover-width-large"))
+    
+        const coverAspectRatio =parseFloat(rootStyles.getPropertyValue
+        ("--book-cover-aspect-ratio")) 
+    
+        const coverHeight =parseFloat(coverwidth/coverAspectRatio) 
+    
+    // Register FilePond plugins
 FilePond.registerPlugin(
     FilePondPluginImagePreview,
     FilePondPluginImageResize,
@@ -6,13 +29,18 @@ FilePond.registerPlugin(
 );
 
 FilePond.setOptions({
-    stylePanelAspectRatio: 150 / 100,
-    imageResizeTargetWidth: 100,
-    imageResizeTargetHeight: 150,
+    stylePanelAspectRatio: 1/ coverAspectRatio,
+    imageResizeTargetWidth: coverwidth,
+    imageResizeTargetHeight: coverHeight ,
 })
 
 // Initialize FilePond
 FilePond.parse(document.body);
+}
+
+
+
+
 
 
 
